a19a6c76d6834d7573bd6ca65b54de244610d22a
KTheES <keunsu2889@gmail.com>
2024-12-31 19:31:24 +0900

    feat: chapter06 생성

--- chapter06/src/test/java/me/kimeunsoo/springbootdeveloper/MemberRepositoryTest.java
+++ chapter06/src/test/java/me/kimeunsoo/springbootdeveloper/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package me.kimeunsoo.springbootdeveloper;
 
+import me.kimeunsoo.springbootdeveloper.Member;
+import me.kimeunsoo.springbootdeveloper.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
